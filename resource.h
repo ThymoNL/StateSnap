1064776a07a0de5c27c5af3ee0eb803a564a2f03
Thymo van Beers <thymovanbeers@gmail.com>
2022-02-27 18:20:56 +0100

    Add module category and description

---
+++ resource.h
@@ -0,0 +1,26 @@
+/*
+ *     StateSnap - A module or Orbiter to periodically make scenario snapshots.
+ *     Copyright (C) 2022  Thymo van Beers
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along
+ *     with this program; if not, write to the Free Software Foundation, Inc.,
+ *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef STATESNAP_RESOURCE_H
+#define STATESNAP_RESOURCE_H
+
+#define IDS_INFO 1000
+#define IDS_TYPE 1001
+
+#endif //STATESNAP_RESOURCE_H
